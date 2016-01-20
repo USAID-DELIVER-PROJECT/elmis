d4cc09e2fdd31d9d3e50d0fed13111482b83b067
Elias Muluneh <eliasmu@gmail.com>
2016-01-20 17:42:47 -0500

    Create a REST-API end point that returns stock status and AMC for a product from IVD form submissions - WIP

---
+++ modules/ivd-form/src/main/java/org/openlmis/ivdform/controller/IvdRestApi.java
@@ -0,0 +1,25 @@
+package org.openlmis.ivdform.controller;
+
+
+import org.openlmis.core.web.OpenLmisResponse;
+import org.openlmis.core.web.controller.BaseController;
+import org.openlmis.ivdform.service.IvdFormService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping(value = "/rest-api/ivd/")
+public class IvdRestApi extends BaseController {
+
+  @Autowired
+  IvdFormService service;
+
+  @RequestMapping(value = "stock-status", method = RequestMethod.GET)
+  public ResponseEntity<OpenLmisResponse> getPeriods(@RequestParam("facilityCode") String facilityCode, @RequestParam("productCode") String productCode, @RequestParam("programCode") String programCode, @RequestParam("periodId") Long periodId) {
+    return OpenLmisResponse.response("status", service.getStockInfoFor(facilityCode, productCode, programCode, periodId));
+  }
+}
