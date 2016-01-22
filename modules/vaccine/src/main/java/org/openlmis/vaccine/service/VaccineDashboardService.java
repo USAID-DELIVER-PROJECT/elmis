8ee2bc76d1fc55702f68bb1fd70dbb441d707d65
Teklu <teklutesfayeh@gmail.com>
2016-01-22 17:48:29 +0300

    add logger for vaccineDashboardService to show when exception occurs

--- modules/vaccine/src/main/java/org/openlmis/vaccine/service/VaccineDashboardService.java
+++ modules/vaccine/src/main/java/org/openlmis/vaccine/service/VaccineDashboardService.java
@@ -14,6 +14,8 @@ package org.openlmis.vaccine.service;
 import lombok.NoArgsConstructor;
 import org.joda.time.format.DateTimeFormat;
 import org.openlmis.vaccine.repository.VaccineDashboardRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,145 +27,329 @@ import java.util.List;
 @Service
 @NoArgsConstructor
 public class VaccineDashboardService {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(VaccineDashboardService.class);
     private static final String DATE_FORMAT = "yyyy-MM-dd";
 
     @Autowired
     VaccineDashboardRepository repository;
 
-    public HashMap<String, Object> getReportingSummary(Long userId){
-        return repository.getReportingSummary(userId);
+    public HashMap<String, Object> getReportingSummary(Long userId) {
+
+        HashMap<String, Object> reportingSummaryList = null;
+        try {
+            reportingSummaryList = repository.getReportingSummary(userId);
+        } catch (Exception ex) {
+            LOGGER.warn("error while loading Reporting summary:... ", ex);
+        }
+        return reportingSummaryList;
     }
 
-    public List<HashMap<String, Object>> getReportingDetails(Long userId){
-        return repository.getReportingDetails(userId);
+    public List<HashMap<String, Object>> getReportingDetails(Long userId) {
+        List<HashMap<String, Object>> reportingDetails=null;
+        try {
+        reportingDetails= repository.getReportingDetails(userId);
+        } catch (Exception ex) {
+            LOGGER.warn("error while loading Reporting summary:... ", ex);
+
+        }
+        return reportingDetails;
     }
 
-    public HashMap<String, Object> getRepairingSummary(Long userId){
-        return repository.getRepairingSummary(userId);
-    }
-
-    public List<HashMap<String, Object>> getRepairingDetails(Long userId){
-        List<HashMap<String, Object>> repairingDetailList=null;
-        try{
+    public HashMap<String, Object> getRepairingSummary(Long userId) {
+        HashMap<String, Object> repairingDetailList = null;
+        try {
 
 
-        repairingDetailList=repository.getRepairingDetails(userId);
-        }catch (Exception ex){
-System.out.println(" ex "+ ex.getMessage());
+            repairingDetailList = repository.getRepairingSummary(userId);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
         }
         return repairingDetailList;
     }
 
-    public HashMap<String, Object> getInvestigatingSummary(Long userId){
-        return repository.getInvestigatingSummary(userId);
+    public List<HashMap<String, Object>> getRepairingDetails(Long userId) {
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
+
+
+            repairingDetailList = repository.getRepairingDetails(userId);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
     }
 
-    public List<HashMap<String, Object>> getInvestigatingDetails(Long userId){
-        return repository.getInvestigatingDetails(userId);
+    public HashMap<String, Object> getInvestigatingSummary(Long userId) {
+        HashMap<String, Object> repairingDetailList = null;
+        try {
+
+
+            repairingDetailList =repository.getInvestigatingSummary(userId);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
     }
 
-    public List<HashMap<String, Object>> getMonthlyCoverage(String startDate, String endDate, Long userId, Long product){
+    public List<HashMap<String, Object>> getInvestigatingDetails(Long userId) {
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
+
+
+            repairingDetailList =repository.getInvestigatingDetails(userId);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
+    }
+
+    public List<HashMap<String, Object>> getMonthlyCoverage(String startDate, String endDate, Long userId, Long product) {
         Date fromDate = DateTimeFormat.forPattern(DATE_FORMAT).parseDateTime(startDate).toDate();
         Date toDate = DateTimeFormat.forPattern(DATE_FORMAT).parseDateTime(endDate).toDate();
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
 
-        return repository.getMonthlyCoverage(fromDate, toDate,userId, product);
-    }
-    public List<HashMap<String, Object>> getFacilityCoverage(Long period, Long product, Long userId){
-        return repository.getFacilityCoverage(period, product, userId);
+
+            repairingDetailList =repository.getMonthlyCoverage(fromDate, toDate, userId, product);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
     }
 
-    public List<HashMap<String, Object>> getFacilityCoverageDetails(String startDate, String endDate, Long product, Long userId){
+    public List<HashMap<String, Object>> getFacilityCoverage(Long period, Long product, Long userId) {
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
+
+
+            repairingDetailList =repository.getFacilityCoverage(period, product, userId);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
+    }
+
+    public List<HashMap<String, Object>> getFacilityCoverageDetails(String startDate, String endDate, Long product, Long userId) {
         Date fromDate = DateTimeFormat.forPattern(DATE_FORMAT).parseDateTime(startDate).toDate();
         Date toDate = DateTimeFormat.forPattern(DATE_FORMAT).parseDateTime(endDate).toDate();
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
 
-        return repository.getFacilityCoverageDetails(fromDate,toDate, product, userId);
+
+            repairingDetailList =repository.getFacilityCoverageDetails(fromDate, toDate, product, userId);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
     }
 
-    public List<HashMap<String, Object>> getFacilitySessions(Long period, Long userId){
-        return repository.getFacilitySessions(period, userId);
+    public List<HashMap<String, Object>> getFacilitySessions(Long period, Long userId) {
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
+
+
+            repairingDetailList =repository.getFacilitySessions(period, userId);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
     }
 
-    public List<HashMap<String, Object>> getFacilitySessionsDetails(String startDate, String endDate, Long userId){
+    public List<HashMap<String, Object>> getFacilitySessionsDetails(String startDate, String endDate, Long userId) {
         Date fromDate = DateTimeFormat.forPattern(DATE_FORMAT).parseDateTime(startDate).toDate();
         Date toDate = DateTimeFormat.forPattern(DATE_FORMAT).parseDateTime(endDate).toDate();
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
 
-        return repository.getFacilitySessionsDetails(fromDate, toDate, userId);
-    }
-    public List<HashMap<String, Object>> getFacilityWastage(Long period, Long product, Long userId){
-        return repository.getFacilityWastage(period, product, userId);
+
+            repairingDetailList =repository.getFacilitySessionsDetails(fromDate, toDate, userId);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
     }
 
-    public List<HashMap<String, Object>> getFacilityWastageDetails(String startDate, String endDate, Long product, Long userId){
+    public List<HashMap<String, Object>> getFacilityWastage(Long period, Long product, Long userId) {
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
+
+
+            repairingDetailList =repository.getFacilityWastage(period, product, userId);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
+    }
+
+    public List<HashMap<String, Object>> getFacilityWastageDetails(String startDate, String endDate, Long product, Long userId) {
         Date fromDate = DateTimeFormat.forPattern(DATE_FORMAT).parseDateTime(startDate).toDate();
         Date toDate = DateTimeFormat.forPattern(DATE_FORMAT).parseDateTime(endDate).toDate();
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
 
-        return repository.getFacilityWastageDetails(fromDate, toDate, product, userId);
+
+            repairingDetailList =repository.getFacilityWastageDetails(fromDate, toDate, product, userId);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
     }
 
-    public List<HashMap<String, Object>> getFacilityDropout(Long period, Long product, Long userId){
-        return repository.getFacilityDropout(period, product, userId);
+    public List<HashMap<String, Object>> getFacilityDropout(Long period, Long product, Long userId) {
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
+
+
+            repairingDetailList = repository.getFacilityDropout(period, product, userId);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
     }
 
-    public List<HashMap<String, Object>> getFacilityDropoutDetails(String startDate, String endDate, Long product, Long userId){
+    public List<HashMap<String, Object>> getFacilityDropoutDetails(String startDate, String endDate, Long product, Long userId) {
         Date fromDate = DateTimeFormat.forPattern(DATE_FORMAT).parseDateTime(startDate).toDate();
         Date toDate = DateTimeFormat.forPattern(DATE_FORMAT).parseDateTime(endDate).toDate();
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
 
-        return repository.getFacilityDropoutDetails(fromDate,toDate, product, userId);
+
+            repairingDetailList = repository.getFacilityDropoutDetails(fromDate, toDate, product, userId);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
     }
 
-    public List<HashMap<String, Object>> getDistrictCoverage(Long period, Long product){
-        return repository.getDistrictCoverage(period, product);
+    public List<HashMap<String, Object>> getDistrictCoverage(Long period, Long product) {
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
+
+
+            repairingDetailList = repository.getDistrictCoverage(period, product);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
     }
 
-    public List<HashMap<String, Object>> getMonthlyWastage(String startDate, String endDate, Long product){
+    public List<HashMap<String, Object>> getMonthlyWastage(String startDate, String endDate, Long product) {
         Date fromDate = DateTimeFormat.forPattern(DATE_FORMAT).parseDateTime(startDate).toDate();
         Date toDate = DateTimeFormat.forPattern(DATE_FORMAT).parseDateTime(endDate).toDate();
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
 
-        return repository.getMonthlyWastage(fromDate, toDate, product);
+
+            repairingDetailList = repository.getMonthlyWastage(fromDate, toDate, product);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
     }
 
-    public List<HashMap<String, Object>> getMonthlySessions(String startDate, String endDate){
+    public List<HashMap<String, Object>> getMonthlySessions(String startDate, String endDate) {
         Date fromDate = DateTimeFormat.forPattern(DATE_FORMAT).parseDateTime(startDate).toDate();
         Date toDate = DateTimeFormat.forPattern(DATE_FORMAT).parseDateTime(endDate).toDate();
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
 
-        return repository.getMonthlySessions(fromDate, toDate);
+
+            repairingDetailList = repository.getMonthlySessions(fromDate, toDate);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
     }
 
-    public List<HashMap<String, Object>> getDistrictSessions(Long period){
-        return repository.getDistrictSessions(period);
+    public List<HashMap<String, Object>> getDistrictSessions(Long period) {
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
+
+
+            repairingDetailList =repository.getDistrictSessions(period);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
     }
 
-    public List<HashMap<String, Object>> getBundling(String startDate, String endDate, Long productId){
+    public List<HashMap<String, Object>> getBundling(String startDate, String endDate, Long productId) {
         Date fromDate = DateTimeFormat.forPattern(DATE_FORMAT).parseDateTime(startDate).toDate();
         Date toDate = DateTimeFormat.forPattern(DATE_FORMAT).parseDateTime(endDate).toDate();
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
 
-        return repository.getBundling(fromDate, toDate, productId);
+
+            repairingDetailList =repository.getBundling(fromDate, toDate, productId);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
     }
 
 
+    public List<HashMap<String, Object>> getWastageByDistrict(Long period, Long product) {
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
 
-    public List<HashMap<String, Object>> getWastageByDistrict(Long period, Long product){
-        return repository.getWastageByDistrict(period, product);
+
+            repairingDetailList =repository.getWastageByDistrict(period, product);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
     }
-    public List<HashMap<String, Object>> getMonthlyDropout(String startDate, String endDate, Long product){
+
+    public List<HashMap<String, Object>> getMonthlyDropout(String startDate, String endDate, Long product) {
         Date fromDate = DateTimeFormat.forPattern(DATE_FORMAT).parseDateTime(startDate).toDate();
         Date toDate = DateTimeFormat.forPattern(DATE_FORMAT).parseDateTime(endDate).toDate();
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
 
-        return repository.getMonthlyDropout(fromDate, toDate, product);
-    }
-    public List<HashMap<String, Object>> getDistrictDropout(Long period, Long product){
-        return repository.getDistrictDropout(period, product);
+
+            repairingDetailList = repository.getMonthlyDropout(fromDate, toDate, product);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
     }
 
-    public List<HashMap<String, Object>> getMonthlyStock(String startDate, String endDate, Long product){
+    public List<HashMap<String, Object>> getDistrictDropout(Long period, Long product) {
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
+
+
+            repairingDetailList = repository.getDistrictDropout(period, product);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
+    }
+
+    public List<HashMap<String, Object>> getMonthlyStock(String startDate, String endDate, Long product) {
         Date fromDate = DateTimeFormat.forPattern(DATE_FORMAT).parseDateTime(startDate).toDate();
         Date toDate = DateTimeFormat.forPattern(DATE_FORMAT).parseDateTime(endDate).toDate();
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
 
-        return repository.getMonthlyStock(fromDate, toDate, product);
+
+            repairingDetailList = repository.getMonthlyStock(fromDate, toDate, product);
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
     }
-    public List<HashMap<String, Object>> getDistrictStock(Long period, Long product){
-        return repository.getDistrictStock(period, product);
+
+    public List<HashMap<String, Object>> getDistrictStock(Long period, Long product) {
+        List<HashMap<String, Object>> repairingDetailList = null;
+        try {
+
+
+            repairingDetailList = repository.getDistrictStock(period, product);;
+        } catch (Exception ex) {
+            LOGGER.warn("error occured.... ", ex);
+        }
+        return repairingDetailList;
     }
 
 }
