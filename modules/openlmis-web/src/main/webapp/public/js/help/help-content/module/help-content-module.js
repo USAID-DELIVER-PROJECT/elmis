9be0adfed99c0e10bc8ea382d502cfcfd957c164
Teklu <teklutesfayeh@gmail.com>
2016-02-12 22:28:38 +0300

    fix help content

--- modules/openlmis-web/src/main/webapp/public/js/help/help-content/module/help-content-module.js
+++ modules/openlmis-web/src/main/webapp/public/js/help/help-content/module/help-content-module.js
@@ -9,17 +9,16 @@
  *
  * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-angular.module('help', ['openlmis','ui.bootstrap.modal', 'ui.bootstrap.dialog', 'ui.bootstrap.dropdownToggle','textAngular']).config(['$routeProvider', function ($routeProvider) {
+angular.module('help', ['openlmis','ui.bootstrap.modal', 'ui.bootstrap.dialog', 'ui.bootstrap.dropdownToggle','textAngular']).
+    config(['$routeProvider', function ($routeProvider) {
 
     $routeProvider.
-        when('/list',{controller: HelpContentController, templateUrl: 'partials/list.html'}).
-        when('/edit/:id', {controller: HelpContentEditCotntroller, templateUrl: 'partials/edit.html'}).
-        when('/create', {controller: HelpContentCreateController, templateUrl: 'partials/create.html'}).
-        when('/viewhelp/:id', {controller: HelpFileContentController, templateUrl: 'partials/helpDetail.html'}).
-        when('/dashboard',{controller: HelpDashboardContentController, templateUrl: 'partials/helpDashboard.html'}).
+
         when('/viewDashboard',{controller: HelpTreeViewController, templateUrl: 'partials/helpView.html'}).
         otherwise({redirectTo: '/viewDashboard'});
+
 }]).run(function ($rootScope, AuthorizationService) {
+
     $rootScope.helpContentSelected = "selected";
 //    AuthorizationService.preAuthorize('MANAGE_PRODUCT');
 });
