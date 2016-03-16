381b514748d670e0db8176b0d2bad66557d1c465
hassan <hhassan.developer@gmail.com>
2016-03-16 13:57:20 +0300

    VIMS-589:- Add Service for Equipment Operational Status CRUD

---
+++ modules/equipment/src/main/java/org/openlmis/equipment/service/EquipmentOperationalStatusService.java
@@ -0,0 +1,42 @@
+package org.openlmis.equipment.service;
+
+import lombok.NoArgsConstructor;
+import org.openlmis.equipment.domain.EquipmentOperationalStatus;
+import org.openlmis.equipment.repository.EquipmentOperationalStatusRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@NoArgsConstructor
+public class EquipmentOperationalStatusService {
+
+
+    EquipmentOperationalStatusRepository repository;
+
+    @Autowired
+   public EquipmentOperationalStatusService(EquipmentOperationalStatusRepository repository){
+        this.repository = repository;
+    }
+
+    public void save(EquipmentOperationalStatus operationalStatus){
+        if(operationalStatus.getId() == null)
+            repository.insert(operationalStatus);
+        else
+            repository.update(operationalStatus);
+    }
+
+    public void remove(Long id){
+        repository.remove(id);
+    }
+
+    public EquipmentOperationalStatus getStatusById(Long id){
+        return repository.getStatusById(id);
+    }
+
+    public List<EquipmentOperationalStatus>getAll(){
+        return repository.getAll();
+    }
+
+}
