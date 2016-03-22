84399dcb3398ad05c38ee82de776ed124e3baf5a
Chrispinus Melkiory <chrismelky05@gmail.com>
2016-03-22 22:00:10 +0300

    rename migrations

--- modules/migration/src/main/resources/db/migration/new_scripts/V714__add_right_to_initialize_stock.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO rights (name, rightType, displaynamekey, displayOrder, description) VALUES
- ('INITIALIZE_STOCK','REQUISITION','right.initialize.stock', 19,'Permission to initialize stock');
