8ed27a8ebd6c522a9d7cedb4385af1be04a0ebf3
hassan <hhassan.developer@gmail.com>
2016-04-29 18:44:20 +0300

    Add Copyright message

--- modules/openlmis-web/src/main/webapp/public/js/vaccine/order-requisition/controller/create-order-requisition-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/vaccine/order-requisition/controller/create-order-requisition-controller.js
@@ -1,4 +1,12 @@
+/*
+ * Electronic Logistics Management Information System (eLMIS) is a supply chain management system for health commodities in a developing country setting.
+ *
+ * Copyright (C) 2015 Clinton Health Access Initiative (CHAI)/MoHCDGEC Tanzania.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the GNU Affero General Public License for more details.
+ */
 
 
 function CreateVaccineOrderRequisition($scope, $dialog,$routeParams, $window, report, VaccineOrderRequisitionSubmit, $location) {
