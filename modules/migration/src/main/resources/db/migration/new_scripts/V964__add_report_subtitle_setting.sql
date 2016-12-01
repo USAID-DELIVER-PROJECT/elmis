464a3d0e639bc0dd56783659233649a3c0ac516c
Teklu <teklutesfayeh@gmail.com>
2016-12-01 09:42:08 +0300

    VIMS-713  All VIMS pdf reports should display three common titles

--- modules/migration/src/main/resources/db/migration/new_scripts/V964__add_report_subtitle_setting.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V964__add_report_subtitle_setting.sql
@@ -1,4 +1,5 @@
 delete from configuration_settings where key = 'REPORT_SUB_TITLE_TEXT';
+delete from configuration_settings where key = 'REPORT_COUNTRY_TITLE_TEXT';
 
 INSERT INTO configuration_settings (key, name, groupname, description, value, valueType, displayOrder)
-  values ('REPORT_SUB_TITLE_TEXT', 'Report Sub Title', 'Report Labels', '','true',  'TEXT', 100);
+  values ('REPORT_COUNTRY_TITLE_TEXT', 'Report Country Title', 'Report Labels', '','The United Republic of Tanzania',  'TEXT', 3);
