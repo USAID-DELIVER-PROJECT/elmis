da426e6a35e0583450cc917234c2b87fa4ce9e2f
hassan <hhassan.developer@gmail.com>
2017-02-07 22:15:26 +0300

    add font end code for vaccine distribution report
    (cherry picked from commit cd81246dd2e7b2d809d5aef44882ed17bdb5eae8)

--- modules/openlmis-web/src/main/webapp/public/js/reports/main/controller/distribution-summary-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/main/controller/distribution-summary-controller.js
@@ -1,46 +1,37 @@
-function DistributionSummaryReportController($scope,DistributionSummaryReport){
+function DistributionSummaryReportController($scope,DistributionSummaryReport) {
 
     $scope.exportReport = function (type) {
         $scope.filter.pdformat = 1;
         var params = jQuery.param($scope.getSanitizedParameter());
         var url = '/reports/download/distribution_summary_report/' + type + '?' + params;
-        window.open(url,"_BLANK");
+        window.open(url, "_BLANK");
     };
-    var arr = [];
-    $scope.datarows = [];
-    DistributionSummaryReport.get({}, function(data) {
-        if (data.pages !== undefined && data.pages.rows !== undefined) {
 
-            var groupByFacility = _.groupBy(data.pages.rows,'storeName');
+    var preparedData = [];
+    $scope.datarows = $scope.dataToShow = [];
 
-            arr =  $.map(groupByFacility,function(value,index){
-                return [value];
-            }).sort(function(a, b) {
-                return  b.length - a.length ;
+
+    DistributionSummaryReport.get({}, function (data) {
+
+        if (data.pages != undefined) {
+
+            preparedData = data.pages.rows;
+
+            $scope.headCells = _.keys(_.groupBy(preparedData, function (item) {
+                return item.product
+            }));
+            $scope.rows = _.groupBy(preparedData, function (item) {
+                return item.storeName;
             });
-            console.log(JSON.stringify(arr));
 
-            $scope.data = arr;
-            $scope.datarows = arr;
+            $scope.sortByProductProp = function (values) {
 
-
-         //   $scope.data = data.pages.rows;
-            $scope.paramsChanged($scope.tableParams);
-        }
-    });
-
-    $scope.OnFilterChanged = function() {
-        // clear old data if there was any
- /*       $scope.data = $scope.datarows = [];
-        $scope.filter.max = 10000;
-        DistributionSummaryReport.get($scope.getSanitizedParameter(), function(data) {
-            if (data.pages !== undefined && data.pages.rows !== undefined) {
-                   console.log(data);
-                $scope.data = data.pages.rows;
-                $scope.paramsChanged($scope.tableParams);
+                return _.sortBy(values, function (value) {
+                    return value.product;
+                });
             }
-        });*/
-    };
+        }
 
 
+    })
 }
