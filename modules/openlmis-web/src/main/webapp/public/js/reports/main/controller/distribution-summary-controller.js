79103163249cb0ea2fbbcfa64598296d76c19b1a
hassan <hhassan.developer@gmail.com>
2017-02-08 23:28:14 +0300

    Font End Code for vaccine distribution summary report

--- modules/openlmis-web/src/main/webapp/public/js/reports/main/controller/distribution-summary-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/main/controller/distribution-summary-controller.js
@@ -1,4 +1,5 @@
-function DistributionSummaryReportController($scope,DistributionSummaryReport) {
+function DistributionSummaryReportController($scope, DistributionSummaryReport, VaccineHomeFacilityIvdPrograms, UserFacilityList, FacilityTypeAndProgramProducts) {
+
 
     $scope.exportReport = function (type) {
         $scope.filter.pdformat = 1;
@@ -7,31 +8,57 @@ function DistributionSummaryReportController($scope,DistributionSummaryReport) {
         window.open(url, "_BLANK");
     };
 
-    var preparedData = [];
-    $scope.datarows = $scope.dataToShow = [];
-
-
-    DistributionSummaryReport.get({}, function (data) {
-
-        if (data.pages != undefined) {
-
-            preparedData = data.pages.rows;
-
-            $scope.headCells = _.keys(_.groupBy(preparedData, function (item) {
-                return item.product
-            }));
-            $scope.rows = _.groupBy(preparedData, function (item) {
-                return item.storeName;
-            });
-
-            $scope.sortByProductProp = function (values) {
-
-                return _.sortBy(values, function (value) {
-                    return value.product;
+    VaccineHomeFacilityIvdPrograms.get({}, function (p) {
+        var programId = p.programs[0].id;
+        UserFacilityList.get({}, function (f) {
+            var facilityId = f.facilityList[0].id;
+            FacilityTypeAndProgramProducts.get({facilityId: facilityId, programId: programId}, function (data) {
+                var facilityProduct = data.facilityProduct;
+                $scope.facilityProduct = facilityProduct.sort(function (a, b) {
+                    return (a.programProduct.product.id > b.programProduct.product.id) ? 1 : ((b.programProduct.product.id > a.programProduct.product.id) ? -1 : 0);
                 });
+            });
+        });
+
+
+    });
+
+    $scope.OnFilterChanged = function () {
+
+        $scope.datarows = $scope.dataToShow = [];
+        $scope.data = $scope.datarows = [];
+        // $scope.filter.max = 10000;
+        // $scope.filter.page = 1;
+
+        DistributionSummaryReport.get($scope.getSanitizedParameter(), function (data) {
+
+            if (data.pages !== undefined) {
+
+                $scope.datarows = data.pages.rows;
+                var distributedFacilities = data.pages.rows;
+                var byFacility = _.groupBy(distributedFacilities, function (f) {
+                    return f.facilityName;
+                });
+                $scope.distributedFacilities = $.map(byFacility, function (value, index) {
+                    return [{"facilityName": index, "products": value}];
+                });
+
+                $scope.paramsChanged($scope.tableParams);
+
             }
-        }
 
+        });
+    };
 
-    })
+    $scope.getProductQuantity = function (facilityName, productName) {
+
+        var f = _.findWhere($scope.distributedFacilities, {facilityName: facilityName});
+        if (f !== undefined)
+            p = _.findWhere(f.products, {product: productName});
+        if (p !== undefined)
+            return p.quantityIssued;
+        else
+            return null;
+    };
+
 }
