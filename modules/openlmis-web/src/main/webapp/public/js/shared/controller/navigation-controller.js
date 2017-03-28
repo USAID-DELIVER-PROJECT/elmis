87ba528cd83783d2ccb4727d6f73b9b4e176a6e0
Adugna Worku <adugna.worku@gmail.com>
2017-03-28 17:53:58 +0300

    Fix navigation menu distortion caused js dependecy resolution problem

--- modules/openlmis-web/src/main/webapp/public/js/shared/controller/navigation-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/shared/controller/navigation-controller.js
@@ -8,7 +8,7 @@
  * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
  */
 
-function NavigationController($scope, ConfigSettingsByKey, localStorageService, Locales, $location, $window, CustomReportList, $timeout) {
+function NavigationController($scope, ConfigSettingsByKey, localStorageService, Locales, $location, $window, CustomReportsList, $timeout) {
 
   ConfigSettingsByKey.get({key: 'LOGIN_SUCCESS_DEFAULT_LANDING_PAGE'}, function (data){
     $scope.homePage =  data.settings.value;
@@ -64,13 +64,13 @@ $scope.homeLinkClicked=function(){
   };
 
   $scope.getCustomReportsList = function() {
-      CustomReportList.get({}, function (list) {
+      CustomReportsList.get({}, function (list) {
           $scope.customReports =_.groupBy(list.reports,'category');
       });
   }();
 
   function hideEmptyReportCategory(){
-      $('#all-report-lists li.children ul li.ng-hide').parent().parent().hide();
+      $('#all-report-lists li.report-children ul li.ng-hide').parent().parent().hide();
   }
   $timeout(hideEmptyReportCategory, 0);
 }
