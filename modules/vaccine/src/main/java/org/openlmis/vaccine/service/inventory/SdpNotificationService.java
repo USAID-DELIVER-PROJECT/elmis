57f44dd3ecd3ff17729d1f9eeb28b9530a371da3
hassan <hhassan.developer@gmail.com>
2017-06-22 21:00:18 +0300

    Add API to send distribution notification to TIMR

--- modules/vaccine/src/main/java/org/openlmis/vaccine/service/inventory/SdpNotificationService.java
+++ modules/vaccine/src/main/java/org/openlmis/vaccine/service/inventory/SdpNotificationService.java
@@ -1,8 +1,14 @@
 package org.openlmis.vaccine.service.inventory;
 
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.util.Base64;
+import org.openlmis.core.domain.ConfigurationSetting;
+import org.openlmis.core.service.ConfigurationSettingService;
 import org.openlmis.vaccine.domain.inventory.VaccineDistribution;
 import org.openlmis.vaccine.repository.mapper.inventory.VaccineInventoryDistributionMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -10,6 +16,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 
 /**
  * Created by hassan on 5/25/17.
@@ -22,34 +34,94 @@ public class SdpNotificationService {
     @Autowired
     private VaccineInventoryDistributionMapper mapper;
 
-    public void updateNotification(Long distributionId){
-        if(distributionId != null) {
+    @Autowired
+    ConfigurationSettingService configurationSettingService;
+
+    public void updateNotification(Long distributionId) {
+        if (distributionId != null) {
             VaccineDistribution d = mapper.getDistributionById(distributionId);
-            sendNotification(d);
+            sendHttps(d);
+            //sendNotification(d);
         }
 
     }
 
-    private void sendNotification(VaccineDistribution d) {
+    public void sendHttps(VaccineDistribution d) {
+        System.out.println("Reached Here");
+      //  configurationSettingService.getByKey("TIMR_INTERGRATION").getValue();
+        String url =
+                "https://ec2-54-187-21-117.us-west-2.compute.amazonaws.com/SVC/HealthFacilityManagement.svc/receiveDelivery?vimsToFacilityId=" + d.getToFacilityId();
+        System.out.println(url);
+        try {
+            URL obj = new URL(url);
+
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager(){
+                public X509Certificate[] getAcceptedIssuers(){return null;}
+                public void checkClientTrusted(X509Certificate[] certs, String authType){}
+                public void checkServerTrusted(X509Certificate[] certs, String authType){}
+            }};
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+            String userCredentials = "lucy:Kaloleni12";
+            String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userCredentials.getBytes()));
+            con.setRequestProperty("Authorization", basicAuth);
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Content-Type", "application/json");
+            System.out.println("all connection"+con);
+
+            int responseCode = con.getResponseCode();
+            System.out.println("\nSending 'GET' request to URL : " + url);
+            System.out.println("Response Code : " + responseCode);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            //print result
+            System.out.println(response.toString());
+        } catch (Exception e) {
+            System.out.println("e"+e.getMessage());
+            e.printStackTrace();
+    }
+
+
+    }
+
+
+   /* private void sendNotification(VaccineDistribution d) {
 
         RestTemplate restTemplate = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
+        headers.add("username", "lucy");
+        headers.add("password", "Kaloleni12");
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> request = new HttpEntity<>(d.toString(), headers);
-        try {
-            ResponseEntity<String> response = restTemplate.postForEntity("https://www.broker.tz.vodafone.com:28443/broker/receive", request, String.class);
-           if(response.getStatusCode().toString().equals("OK")){
-               //update sent notification
+        String url =
+                "https://ec2-54-187-21-117.us-west-2.compute.amazonaws.com/SVC/HealthFacilityManagement.svc/receiveDelivery?vimsToFacilityId=" + d.getToFacilityId();
 
-           }
-        }catch (Exception e){
+        try {
+            ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
+            if (response.getStatusCode().toString().equals("OK")) {
+                //update sent notification
+
+            }
+        } catch (Exception e) {
 
             System.out.println(e.getMessage());
         }
         System.out.println(d);
 
-    }
+    }*/
 
 
 }
