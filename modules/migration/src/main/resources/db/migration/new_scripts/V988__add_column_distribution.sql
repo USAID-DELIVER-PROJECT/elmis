8958e16ef57574f98678f263e9376f8e01c9c53f
hassan <hhassan.developer@gmail.com>
2017-06-23 12:43:51 +0300

    Fix column already exist issue

--- modules/migration/src/main/resources/db/migration/new_scripts/V988__add_column_distribution.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V988__add_column_distribution.sql
@@ -1,1 +1,9 @@
-ALTER TABLE vaccine_distributions ADD COLUMN isNotificationSent Boolean default false;
+DO $$
+    BEGIN
+        BEGIN
+            ALTER TABLE vaccine_distributions ADD COLUMN isNotificationSent Boolean default false;
+        EXCEPTION
+            WHEN duplicate_column THEN RAISE NOTICE 'column isNotificationSent already exists in vaccine_distributions.';
+        END;
+    END;
+$$
