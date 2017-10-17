a1fa5b7fd32f38782975a470bbbc3af9d2fd68bc
hassan <hhassan.developer@gmail.com>
2017-10-17 23:52:35 +0300

    Fix right test issue

--- modules/core/src/test/java/org/openlmis/core/repository/mapper/RightMapperIT.java
+++ modules/core/src/test/java/org/openlmis/core/repository/mapper/RightMapperIT.java
@@ -97,7 +97,7 @@ public class RightMapperIT {
     List<Right> rights = rightMapper.getAll();
 
 
-    assertThat(rights.size(), is(116));
+    assertThat(rights.size(), is(117));
 
     assertThat(rights.get(0).getDisplayOrder(),is(1));
   }
