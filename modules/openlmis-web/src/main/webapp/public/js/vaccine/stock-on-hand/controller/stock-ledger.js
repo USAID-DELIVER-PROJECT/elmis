2c7c3ea05c389fefaf94e2749df13a4a2de40ca1
hassan <hhassan.developer@gmail.com>
2017-10-17 21:02:50 +0300

    Add stock ledger report for stock on hand  analytical report

--- modules/openlmis-web/src/main/webapp/public/js/vaccine/stock-on-hand/controller/stock-ledger.js
+++ modules/openlmis-web/src/main/webapp/public/js/vaccine/stock-on-hand/controller/stock-ledger.js
@@ -1,50 +1,67 @@
-function StockLedgerFunction($scope,$window, $filter, $stateParams,StockLedgerReport){
-   // $scope.userName='Hassan';
-    $scope.idElement = $stateParams.idElement;
-    console.log($stateParams);
+function StockLedgerFunction($scope, $window, $filter, $stateParams, StockLedgerReport) {
 
-console.log($stateParams);
+    $scope.stateParams = $stateParams;
+    var endYear = new Date(new Date().getFullYear(), 11, 31);
+    var starYear = new Date(new Date().getFullYear(), 0, 1);
 
 
-    /*if($scope.filter==undefined || $scope.filter == undefined ){
+    var param = {
+        product: parseInt($scope.stateParams.idElement, 10),
+        facility: parseInt($scope.stateParams.facilityId, 10),
+        startDate: $filter('date')(starYear, "yyyy-MM-dd"),
+        endDate: $filter('date')(endYear, "yyyy-MM-dd"), max: 10000
+    };
+
+    if ($scope.filter == undefined || $scope.filter == undefined) {
 
         $scope.data = $scope.datarows = [];
-        $scope.filter.max = 10000;
 
-        $scope.filter.startDate = $filter('date')($scope.filter.startTime, "yyyy-MM-dd");
-
-        $scope.filter.endDate = $filter('date')($scope.filter.endTime, "yyyy-MM-dd");
-        $scope.product= $stateParams.idElement;
-        StockLedgerReport.get($scope.getSanitizedParameter(), function (data) {
+        StockLedgerReport.get(param, function (data) {
             if (data.pages !== undefined && data.pages.rows !== undefined) {
                 $scope.data = data.pages.rows;
                 $scope.paramsChanged($scope.tableParams);
             }
         });
 
-    }*/
+        $scope.isFilterChanged = false;
 
+    }
 
-
-
-
-    $scope.exportReport = function (type) {
-        $scope.filter.pdformat = 1;
-        var params = jQuery.param($scope.getSanitizedParameter());
+    $scope.exportReport1 = function (type) {
+        $scope.pdformat = 1;
+        var params = jQuery.param(param);
         var url = '/reports/download/stock_ledger/' + type + '?' + params;
         $window.open(url, "_BLANK");
     };
 
+
     $scope.OnFilterChanged = function () {
+
+        $scope.isFilterChanged = true;
+
+        var endYear = new Date($scope.filter.year, 11, 31);
+        var starYear = new Date($scope.filter.year, 0, 1);
+
+        var param2 = {
+            product: parseInt($scope.stateParams.idElement, 10),
+            facility: parseInt($scope.stateParams.facilityId, 10),
+            startDate: $filter('date')(starYear, "yyyy-MM-dd"),
+            endDate: $filter('date')(endYear, "yyyy-MM-dd"), max: 10000
+        };
+
+        $scope.exportReport2 = function (type) {
+            $scope.pdformat = 1;
+            var params = jQuery.param(param2);
+            var url = '/reports/download/stock_ledger/' + type + '?' + params;
+            $window.open(url, "_BLANK");
+        };
+
+
         // clear old data if there was any
         $scope.data = $scope.datarows = [];
         $scope.filter.max = 10000;
 
-        $scope.filter.startDate = $filter('date')($scope.filter.startTime, "yyyy-MM-dd");
-
-        $scope.filter.endDate = $filter('date')($scope.filter.endTime, "yyyy-MM-dd");
-
-        StockLedgerReport.get($scope.getSanitizedParameter(), function (data) {
+        StockLedgerReport.get(param, function (data) {
             if (data.pages !== undefined && data.pages.rows !== undefined) {
                 $scope.data = data.pages.rows;
                 $scope.paramsChanged($scope.tableParams);
@@ -58,7 +75,7 @@ console.log($stateParams);
     };
 
 
-    $scope.showPopover=false;
+    $scope.showPopover = false;
 
     $scope.popover = {
         title: 'Manufacturer Name',
