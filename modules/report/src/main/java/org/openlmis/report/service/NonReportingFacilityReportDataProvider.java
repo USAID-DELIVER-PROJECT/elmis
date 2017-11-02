e20b6b75ffba369302796c7af199007a66450135
Adugna Worku <adugna.worku@gmail.com>
2017-11-02 10:01:13 +0300

    Non reporting report fix number of expected facilities calculation

--- modules/report/src/main/java/org/openlmis/report/service/NonReportingFacilityReportDataProvider.java
+++ modules/report/src/main/java/org/openlmis/report/service/NonReportingFacilityReportDataProvider.java
@@ -102,8 +102,8 @@ public class NonReportingFacilityReportDataProvider extends ReportDataProvider {
         List<NonReportingFacilityDetail> nonReportingFacilityDetails = reportMapper.getNonReportingFacilities(nonReportingFacilityParam, rowBounds, this.getUserId());
         Double nonReporting = Double.parseDouble(String.valueOf(nonReportingFacilityDetails.size()));
 
-        List<NonReportingFacilityDetail> reportingFacilities = reportMapper.getReportingFacilities(nonReportingFacilityParam, rowBounds, this.getUserId());
-        nonReportingFacilityDetails.addAll(reportingFacilities);
+        //List<NonReportingFacilityDetail> reportingFacilities = reportMapper.getReportingFacilities(nonReportingFacilityParam, rowBounds, this.getUserId());
+        //nonReportingFacilityDetails.addAll(reportingFacilities);
 
         report.setDetails(nonReportingFacilityDetails);
 
@@ -116,6 +116,7 @@ public class NonReportingFacilityReportDataProvider extends ReportDataProvider {
 
         report.setKeyValueSummary(new HashedMap(){{
             put(PERIODS_FOR_CHART, reportMapper.getPeriodsTicksForChart(nonReportingFacilityParam));
+            put("chartData", reportMapper.getReportingStatusChartData(nonReportingFacilityParam, rowBounds, getUserId()));
         }});
 
 
