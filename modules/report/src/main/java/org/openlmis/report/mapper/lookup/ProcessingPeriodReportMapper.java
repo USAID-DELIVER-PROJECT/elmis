e154e1bfc512fb4dcef2fe00c34a5559cb738412
Teklu <teklutesfayeh@gmail.com>
2018-01-15 16:54:21 +0300

    load program reference data look

--- modules/report/src/main/java/org/openlmis/report/mapper/lookup/ProcessingPeriodReportMapper.java
+++ modules/report/src/main/java/org/openlmis/report/mapper/lookup/ProcessingPeriodReportMapper.java
@@ -66,6 +66,14 @@ public interface ProcessingPeriodReportMapper {
             "order by pp.startdate desc\n" +
             "limit (select COALESCE(value::integer, 4) from configuration_settings where key ='PROGRAM_VIEWABLE_MAX_LAST_PERIODS')\n")
     List<ProcessingPeriod> getLastPeriods(@Param("programId")Long programId);
-
+    @Select("SELECT pp.* " +
+            "       FROM " +
+            "           processing_schedules s " +
+            "         inner join processing_periods pp ON pp.scheduleid = s.id " +
+            "       where s.id in " +
+            "             (select scheduleId from requisition_group_program_schedules sc " +
+            "             join programs p on p.id = sc.programid where p.code = #{program}) " +
+            "         order by name")
+    List<ProcessingPeriod> getPeriodsByProgramCode(String code);
 }
 
