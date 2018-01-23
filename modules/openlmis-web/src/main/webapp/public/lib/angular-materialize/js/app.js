74285ccfd0a6a2fcefdad2e3cc93a06d17b46ab5
hassan <hhassan.developer@gmail.com>
2018-01-23 22:32:53 +0300

    Add library and images for vims Dashboard

---
+++ modules/openlmis-web/src/main/webapp/public/lib/angular-materialize/js/app.js
@@ -0,0 +1,67 @@
+var app = angular.module('materializeApp', ['ui.materialize'])
+    .controller('BodyController', ["$scope", function ($scope) {
+        $scope.select = {
+            value1: "Option1",
+            value2: "I'm an option",
+            choices: ["Option1", "I'm an option", "This is materialize", "No, this is Patrick."]
+        };
+
+        $scope.dummyInputs = {};
+
+    }])
+    .controller('CollapsibleController', ["$scope", function ($scope) {
+        $scope.collapsibleElements = [{
+            icon: 'mdi-image-filter-drama',
+            title: 'First',
+            content: 'Lorem ipsum dolor sit amet.'
+        },{
+            icon: 'mdi-maps-place',
+            title: 'Second',
+            content: 'Lorem ipsum dolor sit amet.'
+        },{
+            icon: 'mdi-social-whatshot',
+            title: 'Third',
+            content: 'Lorem ipsum dolor sit amet.'
+        }];
+    }]).controller('ToastController', ["$scope", function ($scope) {
+        $scope.callback = function(message) {
+            alert(message);
+        };
+    }]).controller('PaginationController', ["$scope", function ($scope) {
+        $scope.changePage = function (page) {
+            Materialize.toast("Changed to page " + page, 1000);
+        }
+    }])
+    .controller('DateController', ["$scope", function ($scope) {
+        var currentTime = new Date();
+        $scope.currentTime = currentTime;
+        $scope.month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        $scope.monthShort = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        $scope.weekdaysFull = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        $scope.weekdaysLetter = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+        $scope.disable = [false, 1, 7];
+        $scope.today = 'Today';
+        $scope.clear = 'Clear';
+        $scope.close = 'Close';
+        var days = 15;
+        $scope.minDate = (new Date($scope.currentTime.getTime() - ( 1000 * 60 * 60 *24 * days ))).toISOString();
+        $scope.maxDate = (new Date($scope.currentTime.getTime() + ( 1000 * 60 * 60 *24 * days ))).toISOString();
+        $scope.onStart = function () {
+            console.log('onStart');
+        };
+        $scope.onRender = function () {
+            console.log('onRender');
+        };
+        $scope.onOpen = function () {
+            console.log('onOpen');
+        };
+        $scope.onClose = function () {
+            console.log('onClose');
+        };
+        $scope.onSet = function () {
+            console.log('onSet');
+        };
+        $scope.onStop = function () {
+            console.log('onStop');
+        };
+    }]);
