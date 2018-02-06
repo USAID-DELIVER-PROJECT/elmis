984ac986249f717bb70e060d347942040a55c5f1
hassan <hhassan.developer@gmail.com>
2018-02-06 08:32:11 +0300

    Add delete cascade for MSD stock status report

--- modules/migration/src/main/resources/db/migration/new_scripts/V1030__add_right_view_msd_dashlet.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V1030__add_right_view_msd_dashlet.sql
@@ -1,3 +1,4 @@
-delete from rights where name = 'VIEW_MSD_DASHLET_REPORT';
+DELETE from role_rights where rightname = 'VIEW_MSD_DASHLET_REPORT';
+DELETE FROM rights where name = 'VIEW_MSD_DASHLET_REPORT';
 INSERT INTO rights (name, rightType, displaynamekey, displayOrder, description) VALUES
 ('VIEW_MSD_DASHLET_REPORT','REPORT','right.view.msd.dashboard', 26,'Permission to view MSD Dashboard');
