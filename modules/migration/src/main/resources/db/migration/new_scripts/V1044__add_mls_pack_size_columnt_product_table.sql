66bbae9bc4112c69b235957f4d89e7923388e35f
Teklu <teklutesfayeh@gmail.com>
2018-03-19 16:35:30 +0200

    DESKZM-1681 Create the Daily Stock Status Report

--- modules/migration/src/main/resources/db/migration/new_scripts/V1044__add_mls_pack_size_columnt_product_table.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V1044__add_mls_pack_size_columnt_product_table.sql
@@ -1,2 +1,7 @@
-ALTER TABLE products
-  ADD COLUMN mslpacksize integer;
+DO $$
+  BEGIN
+ALTER TABLE products  ADD COLUMN mslpacksize integer;
+    EXCEPTION
+ WHEN duplicate_column THEN RAISE NOTICE 'column createdby already exists in products';
+  END;
+$$;
