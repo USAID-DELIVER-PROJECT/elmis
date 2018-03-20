0886500782affd90d2645d16c16ffb8ca2d368a9
hassan <hhassan.developer@gmail.com>
2018-03-20 12:10:39 +0300

    Fix dublicate of created date in Rejected rnr Summary

--- modules/report/src/main/java/org/openlmis/report/builder/RejectedRnRReportQueryBuilder.java
+++ modules/report/src/main/java/org/openlmis/report/builder/RejectedRnRReportQueryBuilder.java
@@ -10,14 +10,17 @@ public class RejectedRnRReportQueryBuilder {
 
         RejectedRnRReportParam filter =(RejectedRnRReportParam)params.get("filterCriteria");
 
-        return " select districtId,district_name districtName,region_name as regionName,zone_name as zoneName, to_char(createdDate, 'yyyy_mm') as Month, count(*) rejectedCount from ( \n" +
-                "    select count(*), max(c.createdDate) as createdDate, rnrid, zone_name,d.district_id districtId,d.district_name,d.region_name from requisition_status_changes c \n" +
-                "    join requisitions r on r.id = c.rnrid \n" +
-                "    join facilities f on f.id = r.facilityid \n" +
-                "    join vw_districts d on d.district_id = f.geographiczoneid \n" +
-                "    where c.status = '"+filter.getStatus()+"' and r.programid ="+filter.getProgram()+" and r.periodId = "+filter.getPeriod()+" group by rnrid, d.zone_name,d.district_id ,d.district_name,d.region_name having count(*) > 1\n" +
-                ") a\n" +
-                "group by districtId, a.district_name,a.region_name, a.zone_name, to_char(createdDate, 'yyyy_mm')\n" +
-                "order by to_char(createdDate, 'yyyy_mm')";
+        return " select districtId,district_name districtName,region_name as regionName,zone_name as zoneName, count(*) rejectedCount from ( \n" +
+                "                  select count(*), rnrid, zone_name,d.district_id districtId,d.district_name,d.region_name from requisition_status_changes c \n" +
+                "                    join requisitions r on r.id = c.rnrid \n" +
+                "                    join facilities f on f.id = r.facilityid \n" +
+                "                    join vw_districts d on d.district_id = f.geographiczoneid \n" +
+                "                    where c.status = '"+filter.getStatus()+"' and r.programid ="+filter.getProgram()+" and r.periodId = "+filter.getPeriod()+" group by rnrid, d.zone_name,d.district_id ,d.district_name,d.region_name having count(*) > 1\n" +
+                "                ) a\n" +
+                "                group by districtId, a.district_name,a.region_name, a.zone_name\n" +
+                "               \n" +
+                "                order by a.region_name, a.district_name";
     }
 }
+
+
