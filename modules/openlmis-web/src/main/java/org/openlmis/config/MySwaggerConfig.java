b2a58ad316dea2c3b840d3f2679db694659a331d
Elias Muluneh <eliasmu@gmail.com>
2018-04-24 09:49:08 +0300

    Enable Swagger2, fix outstanding issues related to migration of swagger version.

--- modules/openlmis-web/src/main/java/org/openlmis/config/MySwaggerConfig.java
+++ modules/openlmis-web/src/main/java/org/openlmis/config/MySwaggerConfig.java
@@ -11,47 +11,62 @@
  */
 package org.openlmis.config;
 
-import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
-import com.mangofactory.swagger.models.dto.ApiInfo;
-import com.mangofactory.swagger.plugin.EnableSwagger;
-import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+import com.fasterxml.classmate.TypeResolver;
+import org.openlmis.core.web.OpenLmisResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.async.DeferredResult;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger
+import java.lang.reflect.WildcardType;
+import java.time.LocalDate;
+
+import static springfox.documentation.schema.AlternateTypeRules.newRule;
+
+@EnableSwagger2
 public class MySwaggerConfig {
 
-  private SpringSwaggerConfig springSwaggerConfig;
+  private static Docket docket;
 
-  @SuppressWarnings("SpringJavaAutowiringInspection")
   @Autowired
-  public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
-    this.springSwaggerConfig = springSwaggerConfig;
-    // hack - remove duplicate handlers that were injected erronously.
-    // this avoids the duplicate api endpoints on the swagger ui.
-    do {
-      this.springSwaggerConfig.swaggerRequestMappingHandlerMappings().remove(1);
-    } while (this.springSwaggerConfig.swaggerRequestMappingHandlerMappings().size() > 1);
-  }
+  TypeResolver typeResolver;
 
   @Bean
-  public SwaggerSpringMvcPlugin customImplementation() {
-    return new SwaggerSpringMvcPlugin(this.springSwaggerConfig)
-            .apiInfo(apiInfo())
-            .swaggerGroup("Rest API")
-            .genericModelSubstitutes(ResponseEntity.class)
-            .includePatterns(".*rest-api.*", ".*/api/.*");
+  public Docket elmisApi() {
+    if (docket == null) {
+      docket = new Docket(DocumentationType.SWAGGER_2)
+          .select()
+          .apis(RequestHandlerSelectors.any())
+          .paths(PathSelectors.ant("/rest-api/**"))
+          .build()
+          .directModelSubstitute(LocalDate.class,
+              String.class)
+          .genericModelSubstitutes(ResponseEntity.class)
+          .alternateTypeRules(
+              newRule(typeResolver.resolve(DeferredResult.class,
+                  typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
+                  typeResolver.resolve(WildcardType.class)))
+          .useDefaultResponseMessages(false)
+          .globalResponseMessage(RequestMethod.GET,
+              java.util.Arrays.asList(new ResponseMessageBuilder()
+                  .code(500)
+                  .message("500 message")
+                  .responseModel(new ModelRef("Error"))
+                  .build()))
+          .enableUrlTemplating(false)
+          .tags(new Tag("ELMIS APIs", "APIs related to eLMIS"))
+      ;
+    }
+    return docket;
   }
 
-  private ApiInfo apiInfo() {
-    return new ApiInfo(
-            "e-LMIS REST API",
-            "Please use this API to connect to the e-LMIS. This service allows you to build applications that directly interact with the e-LMIS",
-            "TOS",
-            "info@elmis-dev.org",
-            "API License",
-            "https://github.com/OpenLMIS/open-lmis/blob/master/LICENSE"
-    );
-  }
 }
