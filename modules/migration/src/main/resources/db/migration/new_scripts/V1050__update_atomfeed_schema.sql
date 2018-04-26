f244d2f972f2d9e11b888c3a8e3426e1b2502e68
Elias Muluneh <eliasmu@gmail.com>
2018-04-26 17:12:08 +0300

    Revise migration script to be compatible to postgres 9.x

--- modules/migration/src/main/resources/db/migration/new_scripts/V1050__update_atomfeed_schema.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V1050__update_atomfeed_schema.sql
@@ -2,26 +2,13 @@
 DROP TABLE IF EXISTS atomfeed.chunking_history;
 
 CREATE TABLE atomfeed.chunking_history (
-  id integer NOT NULL,
+  id SERIAL PRIMARY KEY ,
   chunk_length bigint,
   start bigint NOT NULL
 );
 
 
 ALTER TABLE atomfeed.chunking_history OWNER TO postgres;
-
-CREATE SEQUENCE atomfeed.chunking_history_id_seq
-  AS integer
-  START WITH 1
-  INCREMENT BY 1
-  NO MINVALUE
-  NO MAXVALUE
-  CACHE 1;
-
-
-ALTER TABLE atomfeed.chunking_history_id_seq OWNER TO postgres;
-
-ALTER SEQUENCE atomfeed.chunking_history_id_seq OWNED BY atomfeed.chunking_history.id;
 
 DROP TABLE IF EXISTS atomfeed.databasechangelog;
 
@@ -55,7 +42,7 @@ ALTER TABLE atomfeed.databasechangeloglock OWNER TO postgres;
 DROP TABLE IF EXISTS atomfeed.event_records ;
 
 CREATE TABLE atomfeed.event_records (
-  id integer NOT NULL,
+  id SERIAL PRIMARY KEY ,
   uuid character varying(40),
   title character varying(255),
   "timestamp" timestamp with time zone DEFAULT CURRENT_TIMESTAMP(6),
@@ -67,51 +54,19 @@ CREATE TABLE atomfeed.event_records (
 );
 
 
-ALTER TABLE atomfeed.event_records OWNER TO postgres;
-
-CREATE SEQUENCE atomfeed.event_records_id_seq
-  AS integer
-  START WITH 1
-  INCREMENT BY 1
-  NO MINVALUE
-  NO MAXVALUE
-  CACHE 1;
-
-
-ALTER TABLE atomfeed.event_records_id_seq OWNER TO postgres;
-
-ALTER SEQUENCE atomfeed.event_records_id_seq OWNED BY atomfeed.event_records.id;
-
 DROP TABLE IF EXISTS atomfeed.event_records_offset_marker;
 
 CREATE TABLE atomfeed.event_records_offset_marker (
-  id integer NOT NULL,
+  id SERIAL PRIMARY KEY ,
   event_id integer,
   event_count integer,
   category character varying(255)
 );
 
-
-ALTER TABLE atomfeed.event_records_offset_marker OWNER TO postgres;
-
-CREATE SEQUENCE atomfeed.event_records_offset_marker_id_seq
-  AS integer
-  START WITH 1
-  INCREMENT BY 1
-  NO MINVALUE
-  NO MAXVALUE
-  CACHE 1;
-
-
-ALTER TABLE atomfeed.event_records_offset_marker_id_seq OWNER TO postgres;
-
-ALTER SEQUENCE atomfeed.event_records_offset_marker_id_seq OWNED BY atomfeed.event_records_offset_marker.id;
-
-
 DROP TABLE IF EXISTS atomfeed.event_records_queue ;
 
 CREATE TABLE atomfeed.event_records_queue (
-  id integer NOT NULL,
+  id SERIAL PRIMARY KEY ,
   uuid character varying(40),
   title character varying(255),
   "timestamp" timestamp with time zone DEFAULT CURRENT_TIMESTAMP(6),
@@ -120,50 +75,6 @@ CREATE TABLE atomfeed.event_records_queue (
   category character varying(255),
   tags character varying(255)
 );
-
-
-ALTER TABLE atomfeed.event_records_queue OWNER TO postgres;
-
-CREATE SEQUENCE atomfeed.event_records_queue_id_seq
-  AS integer
-  START WITH 1
-  INCREMENT BY 1
-  NO MINVALUE
-  NO MAXVALUE
-  CACHE 1;
-
-
-ALTER TABLE atomfeed.event_records_queue_id_seq OWNER TO postgres;
-
-ALTER SEQUENCE atomfeed.event_records_queue_id_seq OWNED BY atomfeed.event_records_queue.id;
-
-ALTER TABLE ONLY atomfeed.chunking_history ALTER COLUMN id SET DEFAULT nextval('atomfeed.chunking_history_id_seq'::regclass);
-
-
-ALTER TABLE ONLY atomfeed.event_records ALTER COLUMN id SET DEFAULT nextval('atomfeed.event_records_id_seq'::regclass);
-
-
-ALTER TABLE ONLY atomfeed.event_records_offset_marker ALTER COLUMN id SET DEFAULT nextval('atomfeed.event_records_offset_marker_id_seq'::regclass);
-
-ALTER TABLE ONLY atomfeed.event_records_queue ALTER COLUMN id SET DEFAULT nextval('atomfeed.event_records_queue_id_seq'::regclass);
-
-ALTER TABLE ONLY atomfeed.chunking_history
-  ADD CONSTRAINT pk_chunking_history PRIMARY KEY (id);
-
-ALTER TABLE ONLY atomfeed.databasechangelog
-  ADD CONSTRAINT pk_databasechangelog PRIMARY KEY (id, author, filename);
-
-ALTER TABLE ONLY atomfeed.databasechangeloglock
-  ADD CONSTRAINT pk_databasechangeloglock PRIMARY KEY (id);
-
-ALTER TABLE ONLY atomfeed.event_records
-  ADD CONSTRAINT pk_event_records PRIMARY KEY (id);
-
-ALTER TABLE ONLY atomfeed.event_records_offset_marker
-  ADD CONSTRAINT pk_event_records_offset_marker PRIMARY KEY (id);
-
-ALTER TABLE ONLY atomfeed.event_records_queue
-  ADD CONSTRAINT pk_event_records_queue PRIMARY KEY (id);
 
 CREATE INDEX event_records_category_idx ON atomfeed.event_records USING btree (category);
 
